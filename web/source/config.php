7d2936665d2d3db3f82333b1a24b6bd97461757a
Maxim Tolstov <146558812+redH0od@users.noreply.github.com>
2025-06-28 05:04:05 +1000

    Update config.php

--- web/source/config.php
+++ web/source/config.php
@@ -1,6 +1,6 @@
 <?php
 $host = getenv('DB_HOST') ?: 'db';
-$db   = getenv('DB_NAME') ?: 'web_app';
+$db   = getenv('DB_NAME') ?: 'tolstov_exam';
 $user = getenv('DB_USER') ?: 'appuser';
 $pass = getenv('DB_PASS') ?: 'apppassword';
 session_start();
